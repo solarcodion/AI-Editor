00748074cd45b9d4b21055282756dffecd9607fb
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-01-28 01:50:44 +0700

    remove auth store

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,6 @@ import {
   type SetStateAction,
   createContext,
   useContext,
-  useState,
 } from "react";
 import { ThemeProvider, useTheme } from "next-themes";
 import { Toaster } from "sonner";
@@ -28,40 +27,6 @@ const ToasterProvider = () => {
   };
   return <Toaster theme={theme} />;
 };
-interface UserType {
-  user: {
-    pk: number;
-    username: string;
-    email: string;
-    id: string;
-    first_name: string;
-    last_name: string;
-    refreshToken: string;
-    accessToken: string;
-  };
-  expires: string;
-}
-const AuthContext = createContext<{
-  userInfo: UserType | null;
-  setUserInfo: React.Dispatch<React.SetStateAction<UserType | null>>;
-} | null>(null);
-
-export function useAuthState() {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useAuthState must be used within an AuthContext.Provider");
-  }
-  return context;
-}
-export const UserInfoProvider = ({ children }: { children: ReactNode }) => {
-  const [userInfo, setUserInfo] = useState<UserType | null>(null);
-
-  return (
-    <AuthContext.Provider value={{ userInfo, setUserInfo }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
 
 const SessionUUIDContext = createContext<string>("");
 // Helper hook to use the SessionUUID context
@@ -76,26 +41,24 @@ export default function Providers({ children }: { children: ReactNode }) {
   const [font, setFont] = useLocalStorage<string>("novel__font", "Default");
   const sessionUUID = uuidv4();
   return (
-    <UserInfoProvider>
-      <SessionProvider>
-        <ThemeProvider
-          attribute="class"
-          enableSystem
-          disableTransitionOnChange
-          defaultTheme="system">
-          <SessionUUIDContext.Provider value={sessionUUID}>
-            <AppContext.Provider
-              value={{
-                font,
-                setFont,
-              }}>
-              <ToasterProvider />
-              {children}
-              <Analytics />
-            </AppContext.Provider>
-          </SessionUUIDContext.Provider>
-        </ThemeProvider>
-      </SessionProvider>
-    </UserInfoProvider>
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        enableSystem
+        disableTransitionOnChange
+        defaultTheme="system">
+        <SessionUUIDContext.Provider value={sessionUUID}>
+          <AppContext.Provider
+            value={{
+              font,
+              setFont,
+            }}>
+            <ToasterProvider />
+            {children}
+            <Analytics />
+          </AppContext.Provider>
+        </SessionUUIDContext.Provider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
