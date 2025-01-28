33f2a49866553242728e2006ae96f8fefac381f6
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-01-28 11:51:40 +0700

    add token user

--- middleware.ts
+++ middleware.ts
@@ -10,13 +10,16 @@ export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   // If the route is protected and the user has no token, redirect to login
-  if (protectedRoutes.some((route) => pathname.startsWith(route)) && !token) {
+  if (
+    protectedRoutes.some((route) => pathname.startsWith(route)) &&
+    !token?.user
+  ) {
     const loginUrl = new URL("/login", req.url);
     return NextResponse.redirect(loginUrl);
   }
 
   // If the route is public and the user has a token, redirect to chat
-  if (publicRoutes.some((route) => pathname.startsWith(route)) && token) {
+  if (publicRoutes.some((route) => pathname.startsWith(route)) && token?.user) {
     const chatUrl = new URL("/chat", req.url);
     return NextResponse.redirect(chatUrl);
   }
