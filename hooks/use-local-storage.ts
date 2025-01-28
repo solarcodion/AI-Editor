2dd3aacf87c4df0c6b6b429970a32241a6299e37
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-01-28 10:23:28 +0700

    setfont type error

--- hooks/use-local-storage.ts
+++ hooks/use-local-storage.ts
@@ -2,25 +2,26 @@ import { useEffect, useState } from "react";
 
 const useLocalStorage = <T>(
   key: string,
-  initialValue: T,
-  // eslint-disable-next-line no-unused-vars
-): [T, (value: T) => void] => {
-  const [storedValue, setStoredValue] = useState(initialValue);
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue: React.Dispatch<React.SetStateAction<T>> = (value) => {
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
+    setStoredValue(valueToStore);
+    window.localStorage.setItem(key, JSON.stringify(valueToStore));
+  };
 
   useEffect(() => {
-    // Retrieve from localStorage
     const item = window.localStorage.getItem(key);
     if (item) {
       setStoredValue(JSON.parse(item));
     }
   }, [key]);
 
-  const setValue = (value: T) => {
-    // Save state
-    setStoredValue(value);
-    // Save to localStorage
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
   return [storedValue, setValue];
 };
 
