77271b948b3ea0ad92551d328c5f2de35b55791c
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-01-30 21:53:36 +0700

    resolve commits

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    # 'auth_app',
+    'auth_app',
     "editor_chat",
     "allauth",
     "allauth.account",
